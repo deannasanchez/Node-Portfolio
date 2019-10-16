782709e645724df5b9ad5717030acbfff9928b10
Dee Lynn <deannalguzman@gmail.com>
2019-10-15 17:13:33 -0700

    added github function

---
+++ index.js
@@ -0,0 +1,131 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const axios = require("axios");
+
+var object
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const colors = {
+    green: {
+        wrapperBackground: "#E6E1C3",
+        headerBackground: "#C1C72C",
+        headerColor: "black",
+        photoBorderColor: "black"
+    },
+    blue: {
+        wrapperBackground: "#5F64D3",
+        headerBackground: "#26175A",
+        headerColor: "white",
+        photoBorderColor: "#73448C"
+    },
+    pink: {
+        wrapperBackground: "#879CDF",
+        headerBackground: "#FF8374",
+        headerColor: "white",
+        photoBorderColor: "#FEE24C"
+    },
+    red: {
+        wrapperBackground: "#DE9967",
+        headerBackground: "#870603",
+        headerColor: "white",
+        photoBorderColor: "white"
+    }
+};
+
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "checkbox",
+            name: "colors",
+            message: "What is your favorite color?",
+            choices: ["green", "blue", "red", "pink"]
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Enter your GitHub Username"
+        }
+    ]);
+}
+
+async function gitHubInfo(username) {
+    try {
+        const queryUrl = `https://api.github.com/users/${username}`;
+        const response = await axios.get(queryUrl)
+        const user = response.data;
+        const stars = await getStars(`${queryUrl}/starred`);
+        const github = {
+            name: user.name,
+            company: user.company,
+            blog: user.blog,
+            bioImg: user.avatar_url,
+            location: user.location,
+            link: user.html_url,
+            numRepos: user.public_repos,
+            stars,
+            followers: user.followers,
+            email: user.email
+        }
+        console.log(github);
+        return github;
+    } catch (err) {
+        console.log(err);
+    }
+ }
+ function getStars(url) {
+    return axios.get(url).then(response => response.data.map(({ name }) => name).length);
+ }
+
+//gitHubInfo("deeannalynn");
+
+function generateHTML(answers) {
+    console.log(answers)
+
+
+    return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+  <div class="jumbotron jumbotron-fluid backgroundColor: ${this.colors}>
+  <div class="container">
+    <h1 class="display-4">Hi! My name is ${answers.name}</h1>
+    <p class="lead">I am from ${answers.location}.</p>
+    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+    <ul class="list-group">
+      <li class="list-group-item">My GitHub username is ${answers.github}</li>
+    </ul>
+  </div>
+</div>
+</body>
+</html>`;
+}
+
+promptUser()
+    .then(async function (answers) {
+        try {
+            const githubbers = await gitHubInfo(answers.github);
+            console.log("githubbers", githubbers);
+
+            //console.log("test", githubbers);
+           // const html = generateHTML(githubbers);
+
+           // return writeFileAsync("index.html", html);
+
+        } catch (err) {
+            console.log(err)
+        }
+    })
+    .then(function () {
+        console.log("Successfully wrote to index.html");
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
